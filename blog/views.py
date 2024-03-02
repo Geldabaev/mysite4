f227d2e6aac6967a190f0f95ebf9c0068b6f5d9a
Ansar <geldabaev@mail.ru>
2024-03-02 11:17:22 +0300

    test

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "blog/index.html")
+
+
+def test_fun(request):
+    return render(request, "blog/test.html")
+
+
+def categ(request):
+    return render(request, "blog/categ.html")
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index),
+    path("test/", test_fun),
+    path("categ/", categ)
 ]
